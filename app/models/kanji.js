0c7b00b46e1f2e4256f17c5e4b6e79b6d9d1c403
kcbobbe <kcbobbe@gmail.com>
2020-04-23 13:02:58 -0400

    linting

--- app/models/kanji.js
+++ app/models/kanji.js
@@ -3,6 +3,7 @@ import Model, { attr } from '@ember-data/model';
 export default class KanjiModel extends Model {
   @attr kanji;
   @attr kmeaning;
+  @attr kgrade;
   @attr kunyomi_ja;
   @attr kunyomi;
   @attr onyomi_ja;
